060fffe5c88bc917580cde4296a4a848d6627deb
Piotr Persona <persona.piotr@gmail.com>
2024-09-16 11:02:25 +0200

    refactor: add docs
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- optional.go
+++ optional.go
@@ -5,21 +5,26 @@ import (
 	"fmt"
 )
 
+// NoneErr will be returned if the optional value is none.
 var NoneErr = errors.New("the value is none")
 
+// Optional is type wrapper for any value
 type Optional[T any] struct {
 	value T
 	some  bool
 }
 
+// Some returns non-empty value using v.
 func Some[T any](v T) Optional[T] {
 	return Optional[T]{value: v, some: true}
 }
 
+// None returns empty value.
 func None[T any]() Optional[T] {
 	return Optional[T]{some: false}
 }
 
+// Get returns value if Some, and error if None.
 func (o *Optional[T]) Get() (T, error) {
 	if !o.some {
 		return o.value, NoneErr
@@ -27,6 +32,7 @@ func (o *Optional[T]) Get() (T, error) {
 	return o.value, nil
 }
 
+// Default returns provided value if None.
 func (o *Optional[T]) Default(d T) T {
 	if !o.some {
 		return d
@@ -34,6 +40,7 @@ func (o *Optional[T]) Default(d T) T {
 	return o.value
 }
 
+// String returns string representation of value.
 func (o Optional[T]) String() string {
 	if o.some {
 		return fmt.Sprintf("%v", o.value)
